70f10b1c8c9342c9b817f0fee4d54e4f0a218015
Ilya Gerasimov <ilya@gera.io>
2020-05-13 19:01:14 +0300

    Game - created

---
+++ src/io/hexlet/xo/model/Game.java
@@ -0,0 +1,26 @@
+package io.hexlet.xo.model;
+
+public class Game {
+
+    private final Player[] players;
+    private final String name;
+    private final Field field;
+
+    public Game(Player[] players, String name, Field field) {
+        this.players = players;
+        this.name = name;
+        this.field = field;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Field getField() {
+        return field;
+    }
+}

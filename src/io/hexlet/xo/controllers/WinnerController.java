2984ddd152db25e0807f7576c287918645f50548
Ilya Gerasimov <ilya@gera.io>
2020-05-16 13:42:39 +0300

    fixes for WinnerController (were fixed the issues, that were discovered during unit testing)

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -16,7 +16,7 @@ public class WinnerController {
                     return field.getFigure(new Point(i,0));
 
             for (int i = 0; i < 3; i++)
-                if (check(field, new Point(i,0), p-> new Point(p.x +1, p.y )))
+                if (check(field, new Point(0,i), p-> new Point(p.x +1, p.y )))
                     return field.getFigure(new Point(0,i));
 
             if (check(field, new Point(0,0), p-> new Point(p.x +1, p.y +1 )))
@@ -38,14 +38,19 @@ public class WinnerController {
         final Point nextPoint = pointGenerator.next(currentPoint);
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
+
+            if (currentFigure == null) {
+                return false;
+            }
+
+            nextFigure = field.getFigure(nextPoint);
         } catch (InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure == null) return false;
-
-        if (currentFigure!= nextFigure) return false;
+        if (currentFigure!= nextFigure) {
+            return false;
+        }
 
         return check(field, nextPoint, pointGenerator);
 
